5f952c96f2db12829530126e5177c530e3790388
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2022-04-22 15:54:24 +0300

    changes to test

--- crawler/extractor.py
+++ crawler/extractor.py
@@ -9,14 +9,22 @@ reddit = praw.Reddit("bot1", user_agent="bot1 user agent")
 print(f"current user: {reddit.user.me()}")
 
 #reddit url
-url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
+#url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
+url = "https://www.reddit.com/r/wallstreetbets/comments/l6ea1b/what_are_your_moves_tomorrow_january_28_2021/"
 submission = reddit.submission(url=url)
 
-posts = []
-for top_level_comment in submission.comments[1:]:
+from praw.models import MoreComments
+
+for top_level_comment in submission.comments:
     if isinstance(top_level_comment, MoreComments):
         continue
-    posts.append(top_level_comment.body)
-posts = pd.DataFrame(posts,columns=["body"])
-print(posts)
+    print(top_level_comment.body)
+
+# posts = []
+# for top_level_comment in submission.comments[1:]:
+#     if isinstance(top_level_comment, MoreComments):
+#         continue
+#     posts.append(top_level_comment.body)
+# posts = pd.DataFrame(posts,columns=["body"])
+# print(posts)
 
