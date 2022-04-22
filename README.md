a8895a7ae311f77cdbc62cb099195a0bcd716be0
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2022-04-22 21:08:16 +0300

    add second level comment extraction

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ OAuth according to praw best practices using praw.ini file in local config direc
 
 prawn.ini template:
 https://praw.readthedocs.io/en/stable/getting_started/configuration/prawini.html#praw-ini
+
+###Extraction
+extraction of comments and sub-comments guide:
+https://praw.readthedocs.io/en/stable/tutorials/comments.html
+
+The submission object can return a CommentForest object which contains all top level comments and their corresponding sub-level comments as well
+https://praw.readthedocs.io/en/stable/code_overview/other/commentforest.html#praw.models.comment_forest.CommentForest
--- crawler/extractor.py
+++ crawler/extractor.py
@@ -2,19 +2,18 @@ import pandas as pd
 import praw
 from praw.models import MoreComments
 
-#use praw.ini file to initialize reddit object
+# use praw.ini file to initialize reddit object
 reddit = praw.Reddit("bot1", user_agent="bot1 user agent")
 
-#testing that it is the correct user
+# testing that it is the correct user
 print(f"current user: {reddit.user.me()}")
 
-#reddit url
-#url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
+# reddit url
+# url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
 url = "https://www.reddit.com/r/wallstreetbets/comments/l6ea1b/what_are_your_moves_tomorrow_january_28_2021/"
 submission = reddit.submission(url=url)
 
-from praw.models import MoreComments
-
+# extracting top level comments using "isinstance"-method
 # for top_level_comment in submission.comments:
 #     if isinstance(top_level_comment, MoreComments):
 #         continue
@@ -25,14 +24,29 @@ from praw.models import MoreComments
 authors = []
 posts = []
 comments = []
+commentsLevelTwo = []
 
+# extracting top level comments
+print(submission.comments.__len__())
 for top_level_comment in submission.comments[1:]:
     if isinstance(top_level_comment, MoreComments):
         continue
     authors.append(top_level_comment.author)
     comments.append(top_level_comment.body)
-    
 
+# extracting second level comments
+submission.comments.replace_more(limit=0)
+for top_level_comment in submission.comments:
+    for second_level_comment in top_level_comment.replies:
+        # print(second_level_comment.body)
+        commentsLevelTwo.append(second_level_comment.body)
+
+# putting lists in pandas df´s
 commentsDF = pd.DataFrame(comments, columns=["body"])
-print(commentsDF)
+comments2DF = pd.DataFrame(commentsLevelTwo, columns=["body"])
 
+# output
+# print(comments2DF)
+# print(commentsDF)
+commentsDF.info()
+comments2DF.info()
