905f204f96a73380f6e80d30d955fe9bb4da28fe
NickGeo1 <nikosgeorgiadis2001@gmail.com>
2022-04-23 15:16:55 +0300

    Some more reddit attributes explored

--- crawler/extractor.py
+++ crawler/extractor.py
@@ -13,14 +13,26 @@ reddit = praw.Reddit(
 print(f"current user: {reddit.user.me()}")
 
 #reddit url
-url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
-submission = reddit.submission(url=url)
+#url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
+#submission = reddit.submission(url=url)
 
-posts = []
-for top_level_comment in submission.comments[1:]:
-    #if isinstance(top_level_comment, MoreComments):
-        #continue
-    posts.append(top_level_comment.body)
-posts = pd.DataFrame(posts,columns=["body"])
+table = []
+reply = []
+submission.comments.replace_more(limit=None) #replace all MoreComments objects with the actuall comments
+for sub in list(reddit.subreddit("wallstreetbets").top("all"))[:5]:
+    posts = []
+    #for top_level_comment in sub.comments.list(): #gets the subcomments of comments in a hierarchical order(First iterate subcomments then next comment)
+    for top_level_comment in sub.comments[1:6]:
+        if isinstance(top_level_comment, MoreComments) or top_level_comment.author == None:
+            continue
+        #posts.append(top_level_comment.body) #get comment's body
+        for r in top_level_comment.replies:
+            if isinstance(r, MoreComments):
+                continue
+            reply.append(r.body)
+        posts.append(top_level_comment.author.name) #get comment's redditor name 
+    table.append(posts)
+posts = pd.DataFrame(table,columns=[str(i+1) for i in range(5)])
 print(posts)
+print(reply)
 
