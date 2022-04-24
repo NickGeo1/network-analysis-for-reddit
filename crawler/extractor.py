bfeca595760191838b887965f92726b069885ce9
NickGeo1 <nikosgeorgiadis2001@gmail.com>
2022-04-24 19:23:35 +0300

    Update extractor.py

--- crawler/extractor.py
+++ crawler/extractor.py
@@ -16,23 +16,52 @@ print(f"current user: {reddit.user.me()}")
 #url = "https://www.reddit.com/r/UpliftingNews/comments/lemy1b/student_who_made_30k_from_gamestop_donates_games/"
 #submission = reddit.submission(url=url)
 
-table = []
-reply = []
-submission.comments.replace_more(limit=None) #replace all MoreComments objects with the actuall comments
-for sub in list(reddit.subreddit("wallstreetbets").top("all"))[:5]:
-    posts = []
-    #for top_level_comment in sub.comments.list(): #gets the subcomments of comments in a hierarchical order(First iterate subcomments then next comment)
-    for top_level_comment in sub.comments[1:6]:
-        if isinstance(top_level_comment, MoreComments) or top_level_comment.author == None:
-            continue
-        #posts.append(top_level_comment.body) #get comment's body
-        for r in top_level_comment.replies:
-            if isinstance(r, MoreComments):
+'''
+    table = []
+    reply = []
+    submission.comments.replace_more(limit=None) #replace all MoreComments objects with the actuall comments
+    for sub in list(reddit.subreddit("wallstreetbets").top("all"))[:5]:
+        posts = []
+        #for top_level_comment in sub.comments.list(): #gets the subcomments of comments in a hierarchical order(First iterate subcomments then next comment)
+        for top_level_comment in sub.comments[1:6]:
+            if isinstance(top_level_comment, MoreComments) or top_level_comment.author == None:
                 continue
-            reply.append(r.body)
-        posts.append(top_level_comment.author.name) #get comment's redditor name 
-    table.append(posts)
-posts = pd.DataFrame(table,columns=[str(i+1) for i in range(5)])
-print(posts)
-print(reply)
+            #posts.append(top_level_comment.body) #get comment's body
+            for r in top_level_comment.replies:
+                if isinstance(r, MoreComments):
+                    continue
+                reply.append(r.body)
+            posts.append(top_level_comment.author.name) #get comment's redditor name 
+        table.append(posts)
+    posts = pd.DataFrame(table,columns=[str(i+1) for i in range(5)])
+    print(posts)
+    print(reply)
+'''
+
+users = []
+for sub in list(reddit.subreddit("wallstreetbets").top("all"))[:1]:
+    '''
+    comments = sub.comments
+    while isinstance(comments[-1], MoreComments):
+        print(list(comments), "---------->1\n")
+        print(f"Replacing with {comments[-1].comments()}\n")
+        comments = comments[:-1] + comments[-1].comments()
+        print(list(comments), "---------->2\n")
+    '''
+        
+    #comments = [comment.comments() if isinstance(comment, MoreComments) else comment for comment in comments]
+    #final_comments = []
+    #comments = [comment.comments() if isinstance(comment, MoreComments) else comment for comment in comments]
+    #limit = len(comments)
+    #for i in range(limit):
+        #if isinstance(comments[i], MoreComments):
+            #comments[i] = comments[]
+    sub.comments.replace_more(limit=None)
+    for top_level_comment in sub.comments:
+        if top_level_comment.author == None:
+            continue
+        if top_level_comment.author.name not in users:
+            users.append(top_level_comment.author.name) #get comment's redditor name 
+#users = pd.DataFrame(users,columns=["users of first post"])
+print(users)
 
