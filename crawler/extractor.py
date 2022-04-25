6bbcc27d09007fa4571cb120bc2401608fe870b0
Martin Pascal Schmauch <mschmauc@mail.uni-mannheim.de>
2022-04-25 14:15:19 +0300

    added 2 level full page crawl - still 413 exception

--- crawler/extractor.py
+++ crawler/extractor.py
@@ -13,40 +13,52 @@ print(f"current user: {reddit.user.me()}")
 url = "https://www.reddit.com/r/wallstreetbets/comments/l6ea1b/what_are_your_moves_tomorrow_january_28_2021/"
 submission = reddit.submission(url=url)
 
-# extracting top level comments using "isinstance"-method
-# for top_level_comment in submission.comments:
-#     if isinstance(top_level_comment, MoreComments):
-#         continue
-#     print(top_level_comment.author.name)
-#
-
-
 authors = []
-posts = []
-comments = []
-commentsLevelTwo = []
+# posts = []
+authorsLevelOne = []
+authorsLevelTwo = []
 
 # extracting top level comments
-print(submission.comments.__len__())
-for top_level_comment in submission.comments[1:]:
-    if isinstance(top_level_comment, MoreComments):
-        continue
-    authors.append(top_level_comment.author)
-    comments.append(top_level_comment.body)
+#print(submission.authorsLevelOne.__len__())
+# for top_level_comment in submission.comments[1:]:
+#     if isinstance(top_level_comment, MoreComments):
+#         continue
+#     # authors.append(top_level_comment.author)
+#     authorsLevelOne.append(top_level_comment.author)
 
-# extracting second level comments
-submission.comments.replace_more(limit=0)
+# extracting first and second level comments
+submission.comments.replace_more(limit=None, threshold=0)
 for top_level_comment in submission.comments:
+    # if isinstance(top_level_comment, MoreComments):
+    #     continue
     for second_level_comment in top_level_comment.replies:
         # print(second_level_comment.body)
-        commentsLevelTwo.append(second_level_comment.body)
+        # if isinstance(second_level_comment, MoreComments):
+        #     continue
+        authorsLevelTwo.append(second_level_comment.author)
+        authorsLevelOne.append(top_level_comment.author)
+
+# BFS over comment forest
+# submission.comments.replace_more(limit=None)
+# comment_queue = submission.comments[:]  # Seed with top-level
+# while comment_queue:
+#     comment = comment_queue.pop(0)
+#     print(comment.author)
+#     comment_queue.extend(comment.replies)
+
+#easier version of BFS code from above
+# submission.comments.replace_more(limit=None, threshold=0)
+# for comment in submission.comments.list():
+#     print(comment.author)
 
 # putting lists in pandas df´s
-commentsDF = pd.DataFrame(comments, columns=["body"])
-comments2DF = pd.DataFrame(commentsLevelTwo, columns=["body"])
+commentsDF = pd.DataFrame(authorsLevelOne, columns=["property"])
+comments2DF = pd.DataFrame(authorsLevelTwo, columns=["property"])
+comments = pd.DataFrame(list(zip(authorsLevelOne, authorsLevelTwo)), columns=["comment author","reply author"])
+# comments = pd.merge(commentsDF, comments2DF)
 
 # output
 # print(comments2DF)
-# print(commentsDF)
-commentsDF.info()
-comments2DF.info()
+print(comments)
+# commentsDF.info()
+# comments2DF.info()
